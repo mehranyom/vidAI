dc0d377175467fc3dd91e378494e8416e52ead71
Ali Adham <ali.adham@studenti.polito.it>
2025-09-27 18:16:31 +0200

    tasks.py

--- src/vidapp/taks.py
+++ src/vidapp/taks.py
@@ -1,6 +1,33 @@
 from celery import shared_task
 from .models import Video
 import yt_dlp
+import os
+from typing import Optional
+from collections.abc import Mapping
+
+
+def yt_extract_meta_data(url: str) -> Optional[Mapping]:
+    ydl_opts = {
+        'quite' : True,
+        'verbose' : False,
+        'noplaylist' : True,
+        'skip_download' : True,
+        'no_warnings' : True
+    }
+    
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+
+    return {
+        'youtube_url' : url,
+        'youtube_id' : info.get('id'),
+        'title' : info.get('title'),
+        'channel_title' : info.get('channel') or info.get('uploader'),
+        "published_at": info.get("upload_date"),  # 'YYYYMMDD' or None
+        "duration_sec": float(info.get("duration") or 0.0),
+        "ext": info.get("ext"),
+    }
 
 @shared_task
-def yt_download(url: str):
+def 
